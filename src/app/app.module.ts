f5204bbe774ee10dd597c7229162a015237ad4e9
Matheus Braga <matheusssbraga@gmail.com>
2018-11-07 21:31:52 -0300

    Iniciando implementação de validação no formulário de cadastro de lançamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
@@ -33,6 +34,7 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
     ButtonModule,
     CalendarModule,
     InputTextModule,
