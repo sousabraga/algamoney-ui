b66ec00ce4795c28df4b2c28312251b66bccd259
Matheus Braga <matheusssbraga@gmail.com>
2018-11-08 21:38:24 -0300

    Extraindo o componente Navbar para o Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
-
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    CoreModule,
     LancamentosModule,
     PessoasModule
   ],
