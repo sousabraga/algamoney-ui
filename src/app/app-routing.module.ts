599607a4259c2dfc21d73e567baa4143b43897b0
Matheus Braga <matheusssbraga@gmail.com>
2018-11-17 14:23:23 -0300

    Criando o módulo app routing para ser dedicado a configuração de rotas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
+  {path: 'lancamentos', component: LancamentosPesquisaComponent},
+  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+  {path: 'lancamentos/:id', component: LancamentoCadastroComponent},
+  {path: 'pessoas', component: PessoasPesquisaComponent},
+  {path: 'pessoas/nova', component: PessoaCadastroComponent},
+  {path: 'pessoas/:id', component: PessoaCadastroComponent},
+  {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
+  {path: '**', redirectTo: 'pagina-nao-encontrada'}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes),
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 import localePt from '@angular/common/locales/pt';
 
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/components/common/api';
 
+import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { AppComponent } from './app.component';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
 registerLocaleData(localePt, 'pt');
-
-const appRoutes: Routes = [
-  {path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
-  {path: 'lancamentos', component: LancamentosPesquisaComponent},
-  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
-  {path: 'lancamentos/:id', component: LancamentoCadastroComponent},
-  {path: 'pessoas', component: PessoasPesquisaComponent},
-  {path: 'pessoas/nova', component: PessoaCadastroComponent},
-  {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-  {path: '**', redirectTo: 'pagina-nao-encontrada'}
-];
 
 @NgModule({
   declarations: [
@@ -40,9 +24,8 @@ const appRoutes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    RouterModule,
-    RouterModule.forRoot(appRoutes),
     ToastModule,
+    AppRoutingModule,
     CoreModule,
     LancamentosModule,
     PessoasModule
