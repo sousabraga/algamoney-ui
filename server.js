bac8bde759c0244108b641ac506902df8d8ec913
Matheus Braga <matheusssbraga@gmail.com>
2018-11-20 17:49:52 -0300

    Configurando um serviço para servir  a aplicação utilizando o framework Express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const folderPath = '/dist/algamoney-ui';
+
+app.use(express.static(__dirname + folderPath));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + folderPath + '/index.html');
+});
+
+app.listen(4200);
