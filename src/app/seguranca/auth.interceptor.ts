b5b2e2d5e16f651e14ac3014c275351379a170b2
Matheus Braga <matheusssbraga@gmail.com>
2018-11-20 11:31:17 -0300

    Concluíndo implementação do interceptor para inserção e validação do access token nas requisições

--- src/app/seguranca/auth.interceptor.ts
+++ src/app/seguranca/auth.interceptor.ts
@@ -3,27 +3,42 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } fro
 
 import { Observable } from 'rxjs';
 
+import { AuthService } from './auth.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptor implements HttpInterceptor {
 
+  constructor(private authService: AuthService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (req.headers.has('Authorization')) {
+    if (req.headers.has('Authorization') && req.headers.get('Authorization').startsWith('Basic')) {
       return next.handle(req);
     } else {
-      const authRequest = req.clone(
-        {
-          headers: new HttpHeaders(
-            {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-            })
-        }
-      );
-
-      return next.handle(authRequest);
+      if (this.authService.isAccessTokenInvalido()) {
+        const teste = this.authService.obterNovoAccessToken()
+          .then(() => {
+            return next.handle(this.requestClone(req));
+          });
+      } else {
+        return next.handle(this.requestClone(req));
+      }
     }
+  }
+
+  private requestClone(req: HttpRequest<any>): HttpRequest<any> {
+    const authRequest = req.clone(
+      {
+        headers: new HttpHeaders(
+          {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+          })
+      }
+    );
+
+    return authRequest;
   }
 
 }
--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -18,13 +18,7 @@ export class AuthService {
   }
 
   login(email: string, senha: string): Promise<any> {
-    const options = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/x-www-form-urlencoded',
-        'Authorization': `Basic YW5ndWxhcjpAbmd1bEByMA==`
-      })
-    };
-
+    const options = this.getOptions();
     const body = `username=${email}&password=${senha}&grant_type=password`;
 
     return this.http.post<any>(`${ALGAMONEY_API}/oauth/token`, body, options)
@@ -40,6 +34,34 @@ export class AuthService {
       });
   }
 
+  obterNovoAccessToken(): Promise<void> {
+    const options = this.getOptions();
+    const body = 'grant_type=refresh_token';
+
+    return this.http.post<any>(`${ALGAMONEY_API}/oauth/token`, body, options)
+      .toPromise()
+      .then(response => {
+        this.armazenarToken(response.access_token);
+
+        console.log('Novo ACCESS TOKEN criado');
+        return Promise.resolve(null);
+      })
+      .catch(error => {
+        console.error('Erro ao renovar token', error);
+        return Promise.resolve(null);
+      });
+  }
+
+  isAccessTokenInvalido(): boolean {
+    const accessToken = localStorage.getItem('access_token');
+
+    return !accessToken || this.jwtHelper.isTokenExpired(accessToken);
+  }
+
+  possuiPermissao(permissao: string): boolean {
+    return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
+  }
+
   private armazenarToken(accessToken: string) {
     localStorage.setItem('access_token', accessToken);
     this.jwtPayload = this.jwtHelper.decodeToken(accessToken);
@@ -51,6 +73,18 @@ export class AuthService {
     if (accessToken) {
       this.armazenarToken(accessToken);
     }
+  }
+
+  private getOptions() {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/x-www-form-urlencoded',
+        'Authorization': `Basic YW5ndWxhcjpAbmd1bEByMA==`
+      }),
+      withCredentials: true
+    };
+
+    return options;
   }
 
 }
